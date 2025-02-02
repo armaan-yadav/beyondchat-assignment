9b12897e0a32117a06673083cc9cd5acde47b3f4
Armaan <yadavarmaan10@gmail.com>
2025-02-02 12:48:18 +0530

    remove unused components

--- src/chatbot/widgets/Dog.tsx
+++ src/chatbot/widgets/Dog.tsx
@@ -1,38 +1,37 @@
-import Groq from "groq-sdk";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 interface Props {
   message: string;
 }
 const Dog = (props: Props) => {
-  const groq = new Groq({
-    apiKey: import.meta.env.VITE_GROQ_API_KEY,
-    dangerouslyAllowBrowser: true,
-  });
+  // const groq = new Groq({
+  //   apiKey: import.meta.env.VITE_GROQ_API_KEY,
+  //   dangerouslyAllowBrowser: true,
+  // });
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [response, setResponse] = useState("");
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [response, setResponse] = useState("");
 
-  async function fetchResponse() {
-    try {
-      setIsLoading(true);
-      const completion = await groq.chat.completions.create({
-        messages: [
-          {
-            role: "user",
-            content: props.message,
-          },
-        ],
-        model: "llama-3.3-70b-versatile",
-      });
-      completion.choices[0].message.content &&
-        setResponse(completion.choices[0].message.content);
-    } catch (error) {
-      console.log(error);
-      setResponse("OOPS! Something went wrong");
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  // async function fetchResponse() {
+  //   try {
+  //     setIsLoading(true);
+  //     const completion = await groq.chat.completions.create({
+  //       messages: [
+  //         {
+  //           role: "user",
+  //           content: props.message,
+  //         },
+  //       ],
+  //       model: "llama-3.3-70b-versatile",
+  //     });
+  //     completion.choices[0].message.content &&
+  //       setResponse(completion.choices[0].message.content);
+  //   } catch (error) {
+  //     console.log(error);
+  //     setResponse("OOPS! Something went wrong");
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // }
   useEffect(() => {
     // if (props.message) {
     //   fetchResponse();
@@ -44,7 +43,7 @@ const Dog = (props: Props) => {
   }, []);
   console.log("from dog", props.message);
 
-  return <div>{isLoading ? "loadinggg" : response}</div>;
+  return <div>{/* {isLoading ? "loadinggg" : response} */}</div>;
 };
 
 export default Dog;
